a9af3634fc1458b89493c2d48c56b6f562bed254
Bobi <felipehhenriquee@gmail.com>
2026-03-16 13:49:57 -0300

    debug: enable full error reports and disable host authorization in production

--- backend/backend/config/environments/production.rb
+++ backend/backend/config/environments/production.rb
@@ -12,9 +12,12 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  # Full error reports are enabled for debugging production setup.
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
+
+  # Allow all hosts (access via IP)
+  config.hosts.clear
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
