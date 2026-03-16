a2a11574b170a8335a6c38416ec54e257bb8387c
Bobi <felipehhenriquee@gmail.com>
2026-03-16 14:50:53 -0300

    chore: cleanup duplicate production config

--- backend/backend/config/environments/production.rb
+++ backend/backend/config/environments/production.rb
@@ -40,9 +40,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Allow asset compilation in production (for Active Admin)
-  config.assets.compile = true
-
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
